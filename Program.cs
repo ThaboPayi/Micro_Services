78a104f7312e5cfa54eaca3d3c923ed4adb21bc1
ThaboPayi <39402777+ThaboPayi@users.noreply.github.com>
2018-05-22 08:31:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Messaging;
+using System.Messaging.MessageQueue;
+using NServiceBus.Serializers.Binary;
+
+namespace Application2
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Wonga.Com");
+
+            MessageQueue MyQueue;
+            MyQueue = new MessageQueue(@".\Private$\MyQueue");
+
+            Message MyMessage = MyQueue.Receive();
+            MyMessage.Formatter = new BinaryMessageFormatter();
+
+            Console.WriteLine(MyMessage.Body.ToString());
+            Console.ReadKey();
+
+        }
+    }
+}
