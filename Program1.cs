18ba8eb2a1abd1899c1e59600c695ebf808aee50
ThaboPayi <39402777+ThaboPayi@users.noreply.github.com>
2018-05-22 08:33:05 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging.MessageQueue;
+
+namespace Application1
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Wonga.Com");
+            Console.WriteLine("Please write a message you want to send");
+            string Message = Console.ReadLine();
+
+            MessageQueue MyQueue;
+            if (MessageQueue.Exists(@".\Private$\MyQueue"))
+            {
+                MyQueue = new MessageQueue(@".\Private$\MyQueue");
+            }
+            else
+            {
+                MyQueue = MessageQueue.Create(@".\Private$\MyQueue");
+            }
+
+            Message MyMessage = new Message();
+
+            MyMessage.Formatter = new BinaryMessageFormatter();
+            MyMessage.Body = Message;
+            MyMessage.Label = "MessagFromApplication1";
+
+            MyQueue.Send(MyMessage);
+            Console.WriteLine("Message Sent");
+            Console.ReadKey();
+        }
+    }
+}
